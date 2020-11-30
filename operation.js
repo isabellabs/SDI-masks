7bd3a2b39a55fffb66848f5a2b4636146bc3f1a9
isabella <30278887+isabellabs@users.noreply.github.com>
2020-11-30 09:24:53 +0000

    adicionei icones no operation e retirei o script e o style_ps

--- operation.js
+++ operation.js
@@ -53,21 +53,24 @@ if(label == 'Mask On'){
   select('#body').class('mask-on-bg');
   select('#showAdvice').class('showAdviceON');
   select('#displayText').class('mask-on-title');
-	select('#showAdvice').html("YOUR MASK IS ON")
+  select('#showAdvice').html("YOUR MASK IS ON")
+  select('#showAdvice').html("<lottie-player src='https://assets9.lottiefiles.com/packages/lf20_aibtg4s1.json' background='transparent' speed='1' style='width: 300px; height: 300px;' loop autoplay></lottie-player>");
 }
 else if(label == 'Mask Off'){
   select('#body').class('mask-off-bg');
   select('#showAdvice').class('showAdviceOFF');
   select('#displayText').class('mask-off-title');
-	select('#showAdvice').html("YOUR MASK IS OFF")
+  select('#showAdvice').html("YOUR MASK IS OFF");
 }
+
 else if(label == 'Mask Wrong'){
   select('#body').class('mask-wrong-bg');
   select('#showAdvice').class('showAdviceWRONG');
   select('#displayText').class('mask-wrong-title');
-	select('#showAdvice').html("YOUR MASK IS WRONG")
+  select('#showAdvice').html("YOUR MASK IS WRONG");
+  select('#showAdvice').html("<lottie-player src='https://assets5.lottiefiles.com/packages/lf20_d7zirht6.json' background='transparent' speed='1' style='width: 300px; height: 300px;' loop autoplay></lottie-player>");
 }
-}
+
 function gotResults(error, result) {
   if (error) {
     console.error(error);
